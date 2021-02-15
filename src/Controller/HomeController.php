40a334fd81a06535f07566c7a1ee80a49a4c78fa
Enrique Villasana <enrique.villasana26@gmail.com>
2021-02-15 18:50:26 -0400

    Working in home View

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
+    /** @var \Doctrine\Persistence\ObjectRepository */
+    private $activiryRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->activiryRepository = $entityManager->getRepository('App:RecentActivity');
+    }
+
     /**
      * @Route("/home", name="home")
      */
     public function index(): Response
     {
+        $activities = $this->activiryRepository->findAll();
+        $data = array();
+        foreach ($activities as $activity) {
+            $data[] = [
+                "id" => $activity->getId(),
+                "email" => $activity->getEmail(),
+                "lastActivity" => $activity->getLastConnection()->format('Y-m-d H:i:s'),
+            ];
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'activities' => $data
         ]);
     }
 }
